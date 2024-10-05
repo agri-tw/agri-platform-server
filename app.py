a792c7d074d7d16224dbbe11e667c116269b10d7
Lun-Tsan <patrick121680@gmail.com>
2024-10-06 07:44:13 +0800

    006

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import os
-
+import pandas as pd
 import flask
 import google.generativeai as genai
 from dotenv import load_dotenv
 from flask_cors import CORS
+import json
+from neuralforecast import NeuralForecast
+import typing_extensions as typing
+from utils import generate_prompt
+from utils import get_rainfall_data
+from utils import get_temperature_data
+from utils import get_year_data
+import utils
 
 CORS_ALLOW_ORIGINS = ["https://firebase.com", "https://agricultural-platform.web.app", "https://localhost:5173"]
 
@@ -27,6 +35,81 @@ def test(name=None):
     args = flask.request.args
     resp_json = {"args": args, "name": name}
     return resp_json, 200
+
+
+
+@app.post("/predict")
+def predict():
+    rainfall_nf = NeuralForecast.load(path='./model/rain_model')
+    temperature_nf = NeuralForecast.load(path='./model/temp_model')
+
+    data, error = utils.get_input()
+    if not data:
+        code, message = error or (400, "Bad request")
+        error_json = {"error": {"code": code, "message": message}}
+        return error_json, 400
+    
+    lat, long, crop, language = data
+
+    rainfall_df = get_rainfall_data(0, long, lat)
+    temperature_df = get_temperature_data(long, lat)
+
+    # Check if data is valid 
+    if rainfall_df.empty or temperature_df.empty:
+        print("invalid data")
+        return 0
+    
+    # Make a forecast in month format
+    
+    rainfall_forecast = rainfall_nf.predict(rainfall_df)
+    temperature_forecast = temperature_nf.predict(temperature_df)
+
+    # Format the forecast into a query
+
+    rainfall_data = get_year_data(rainfall_forecast, rainfall_df)
+    rainfall_data = rainfall_data.rename(columns={'y': 'Rainfall'})
+    temperature_data = get_year_data(temperature_forecast, temperature_df)
+    temperature_data = temperature_data.rename(columns={'y': 'Temperature'})
+    
+    all_data = pd.merge(rainfall_data, temperature_data, on='ds')
+    all_data = all_data.rename(columns={'ds': 'Date'})
+    all_data.loc[all_data['Rainfall'] < 0, 'Rainfall'] = 0.0
+
+    prompt = generate_prompt(all_data, crop, language)
+    print(prompt)
+    class Report(typing.TypedDict):
+        action_you_should_take_immediately: str
+        water_resoure_management: str
+        irrigation_management: str
+        temperature_management: str
+        pest_and_disease_management: str
+        soil_and_nutrient_management: str
+        crop_selection_and_rotation: str
+        harvest_and_post_harvest_management: str
+
+    # prompt = """You are an expert in agriculture. Suggest some risk mitigation strategies based on the data:
+    #     Crop type: corn
+    #     Past Precipitation: 100mm
+    #     Future Precipitation: 200mm
+    #     Past Temperature: 30C
+    #     Future Temperature: 35C
+    #     Past Soil Moisture: 50%
+    #     Future Soil Moisture: 40%
+    #     format should be:
+    #     1. Irrigation Management
+    #     2. Temperature Management
+    #     3. Pest and Disease Management
+    #     4. Soil and Nutrient Management
+    #     5. Crop Selection and Rotation
+    #     6. Harvest and Post-Harvest Management
+    # """
+    # Get response from gemini
+    raw_response = model.generate_content(
+        prompt,
+        generation_config=genai.GenerationConfig(response_mime_type="application/json", response_schema=Report),
+    )
+    response = json.loads(raw_response.text)
+    return response, 200
 
 
 @app.get("/genai/test")
---
+++ utils.py
@@ -0,0 +1,127 @@
+import flask
+import pandas as pd
+import requests
+import climateserv
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+def get_input():
+    if flask.request.is_json:
+        data = flask.request.get_json()
+        if not data:
+            return None, (400, "Wrong Format")
+    else:
+        return None, (400, "No json")
+
+    lat = data["lat"]
+    long = data["long"]
+    crop = data["crop"]
+    language = data["language"]
+
+    # Load data based on latitude and longitude
+    resolution = 0.25
+    lat = round(lat / resolution) * resolution
+    long = round(long / resolution) * resolution
+    data = lat, long, crop, language
+
+    return data, None
+
+def query_firestore_by_value(db, collection_name, field_name, value):
+    
+    # Perform the query using the 'where' clause
+    docs = db.collection(collection_name).where(field_name, '==', value).stream()
+
+    # Convert Firestore documents to a list of dictionaries
+    data = []
+    for doc in docs:
+        doc_dict = doc.to_dict()  # Convert document to dictionary
+        data.append(doc_dict)     # Append dictionary to the list
+
+    # Convert the list of dictionaries to a pandas DataFrame
+    df = pd.DataFrame(data)
+
+    return df
+
+def get_year_data(forecast, past):
+    forecast = forecast.rename(columns = {"AutoNLinear": "y"})
+    base_date = forecast["ds"].min()
+    past_date = base_date - relativedelta(months=6)
+    past_six_month = past[past["ds"] >= past_date]
+    df = pd.concat([past_six_month, forecast], ignore_index=True)
+    return df
+
+def generate_prompt(df, crop, language):
+    today = date.today()
+    prompt = f"""You are an expert in agriculture.
+Today's date is {today}.
+The crop type is {crop}. 
+Write a easy to understand document in {language} aiming to suggesting risk mitigation strategies and implement sustainable farming practices based on the following 18 months of data:
+Give more concrete advice, tell us what to do rather than explanation.(example: action_you_should_take_immediately: rush in the harvest)
+Write in columnar format. Be specific about the strategies. Your decision is very important and may affect people's lives.\n"""
+    for index, row in df.iterrows():
+        prompt += (
+            f"Date: {row['Date']} | Avg_Temperature: {row['Temperature']}°C | "
+            f"Rainfall_of_the_month: {row['Rainfall']*30} mm \n"
+        )
+    return prompt
+
+def get_rainfall_data(data_type,long, lat):
+    GeometryCoords = [[long-.01,lat+.01],[long+.01, lat+.01],
+                    [long+.01, lat-.01],[long-.01,lat-.01],[long-.01,lat+.01]]
+    
+    resolution = 0.1
+    lat = round (lat / resolution) * resolution
+    long = round (long/ resolution) * resolution
+    DatasetType = data_type
+    OperationType = 'Average'
+    EarliestDate = '08/31/2023'
+    LatestDate = '08/31/2024'
+    SeasonalEnsemble = '' # Leave empty when using the new integer dataset IDs
+    SeasonalVariable = '' # Leave empty when using the new integer dataset IDs
+    Outfile = 'memory_object'
+
+    raw_data = climateserv.api.request_data(DatasetType, OperationType, 
+                EarliestDate, LatestDate,GeometryCoords, 
+                SeasonalEnsemble, SeasonalVariable,Outfile)
+    avg =  [long['value']['avg'] for long in raw_data['data']]
+    date = [long['date'] for long in raw_data['data']]
+    unique_id = [0] * len(avg)
+    df = pd.DataFrame({"unique_id": unique_id, "y": avg, "ds": date})
+    df["ds"] = pd.to_datetime(df["ds"])
+
+    invalid_index = df[df["y"] == -9999].index
+    df.drop(invalid_index, inplace = True)
+    df = df.resample('M', on='ds').mean().reset_index()
+    df['ds'] = df['ds'].apply(lambda x: x.replace(day=1))
+    return df   
+
+def get_temperature_data(long, lat):
+    start_date = '20230101'
+    end_date = '20240831'
+    parameters = 'T2M'
+    url = (
+        'https://power.larc.nasa.gov/api/temporal/daily/point'
+        f'?start={start_date}'
+        f'&end={end_date}'
+        f'&latitude={lat}'
+        f'&longitude={long}'
+        f'&parameters={parameters}'
+        '&community=AG'
+        '&format=JSON'
+    )
+    # Make the API request
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract temperature data
+    temperature_data = data['properties']['parameter']['T2M']
+    dates = list(temperature_data.keys())
+    temperatures = list(temperature_data.values())
+    unique_id = [0] * len(dates)
+    df = pd.DataFrame({"unique_id": unique_id, "y": temperatures, "ds": dates})
+    invalid_index = df[df["y"] == -999].index
+    df.drop(invalid_index, inplace = True)
+    df["ds"] = pd.to_datetime(df["ds"])
+    df = df.resample('M', on='ds').mean().reset_index()
+    df['ds'] = df['ds'].apply(lambda x: x.replace(day=1))
+    return df
